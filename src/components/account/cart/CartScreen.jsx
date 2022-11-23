cd9a8becdf4048c5091c5dd93583191d6caedb5f
WhirlyByrd <kara.r.gardner@gmail.com>
2022-11-23 15:21:45 -0600

    cart calculates subtotal

--- src/components/account/cart/CartScreen.jsx
+++ src/components/account/cart/CartScreen.jsx
@@ -2,19 +2,16 @@ import {useState, useEffect, useContext, useCallback} from 'react'
 import axios from 'axios'
 import {useParams} from 'react-router-dom'
 import AuthContext from '../../../store/authContext'
-import {Card, Button} from 'react-bootstrap'
+import {Container, Col, Row, Card, Button, Image} from 'react-bootstrap'
+import './CartScreen.css'
 
 function CartScreen() {
   const {token, userId} = useContext(AuthContext)
   const [products, setProducts] = useState([])
-  //const [quantity, setQuantity] = useState(1)
+  
   
   
   let url = "http://localhost:4545"
-
-
-  
-
 
   //get products
   const getProducts = useCallback(() => {
@@ -38,9 +35,6 @@ function CartScreen() {
   }, [userId])
   
 
-
-
-
   //delete product
   const deleteCartItem = (cartId) => {
     
@@ -55,18 +49,10 @@ function CartScreen() {
       .catch(err => {console.log(err)})
   }
 
-  //update quantity of product, if same product id is added (add that to quantity field)
-
-  //if + button add 1, if - button subtract 1
-  // let incrementQuantity = () => setQuantity(quantity + 1);
-  // let decrementQuantity = () => setQuantity(quantity - 1);
-  //   if (quantity <= 0) {
-  //   decrementQuantity = () => setQuantity(1)
-  //     }
-
-  
-
+  //update quantity of product & price?
   const editCart = (cartId, quantity) => {
+
+    //let price = 
     
     axios.put(`${url}/carts/${cartId}`, {quantity}, {
       headers: {
@@ -78,11 +64,8 @@ function CartScreen() {
     })
   }
 
-  //add price total of all products in cart
 
-  //clear cart
- 
-  //can I add more functions to useEffect
+
   useEffect(() => {
     getProducts()
   }, [])
@@ -99,24 +82,28 @@ const mappedProducts = products.map(product => {
     return (
      
         
-      <Card style={{ width: '20rem' }} key={product.cartId} >
+      <Card style={{ width: '18rem' }} key={product.cartId} >
       
-        <img className="card-img-top" src={product.img} alt={product.name}/>
-        <div className="card-body">
+      <Image className='card-image' src={product.img} alt={product.name}/>
+        <Card.Body>
           <Card.Title>{product.name}</Card.Title>
-          <h4 className="card-title">${product.price}</h4>
+          <h4 className="card-title">${product.price * product.quantity}</h4>
           <Card.Text className="card-text">{product.desc}</Card.Text>
+          </Card.Body>
 
-
-
-
-          <Button onClick={() => editCart(product.cartId, +product.quantity + 1)}>+</Button>
-          <Card.Text className="card-text">{product.quantity}</Card.Text>
-
-          {product.quantity <= 1 ? (null) : (<Button onClick={() => editCart(product.cartId, +product.quantity - 1)}>-</Button>)} 
+          <div className='card-btn-row'>
+          <div className='quantity-btns'>
+          <Button variant="secondary" onClick={() => editCart(product.cartId, +product.quantity + 1)}>+</Button>
           
-          <Button onClick={() => deleteCartItem(product.cartId)}>Delete</Button>
-        </div>
+          <div className='quantity'>{product.quantity}</div>
+          
+
+          {product.quantity <= 1 ? (null) : (<Button variant="secondary" onClick={() => editCart(product.cartId, +product.quantity - 1)}>-</Button>)} 
+          </div>
+          
+          <Button variant="secondary" onClick={() => deleteCartItem(product.cartId)}>Delete</Button>
+          </div>
+       
       </Card>
      
     );
@@ -124,13 +111,44 @@ const mappedProducts = products.map(product => {
 
   return mappedProducts.length >= 1 ? (
     <main>
-      <div><h1>My Cart</h1></div>
-      <div>{mappedProducts}</div>
+      <Container>
+        
+      <div className='cart-items'>
+
+      <div className='title'>
+        <h1>My Cart</h1>
+      </div>
+      
+      <div>
+        {mappedProducts}
+      </div>
+
+      </div>
+     
+
+      <Card className='cart-summary'>
+        <Card.Title>Cart Summary</Card.Title>
+        <Card.Text>By clicking check out, I agree to the terms & conditions and understand that all sales are final. Some restrictions apply for free shipping. Any applicable discounts or coupons will be reflected at checkout.
+        </Card.Text>
+
+        <div>
+          <Card.Subtitle>Subtotal</Card.Subtitle>
+          <Card.Text>${products.reduce((total, el) => total + (el.price * el.quantity), 0)}</Card.Text>
+          <Button variant="dark">Check Out</Button>
+        </div>
+
+      </Card>
+      
+    
+      
+      </Container>
     </main>
   ) : (
     <main>
+      <Container>
       <div><h1>My Cart</h1></div>
       <h2>Cart is empty</h2>
+      </Container>
     </main>
   )
 }
