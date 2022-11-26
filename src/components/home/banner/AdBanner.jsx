991492534a3cbb3dc84b3646a4881f8efc20bc8c
WhirlyByrd <kara.r.gardner@gmail.com>
2022-11-26 14:13:58 -0600

    home banner styled

--- src/components/home/banner/AdBanner.jsx
+++ src/components/home/banner/AdBanner.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import {Container} from 'react-bootstrap'
+import './AdBanner.css'
+import {useNavigate} from 'react-router-dom'
+import AuthContext from '../../../store/authContext'
 
 function AdBanner() {
-  return (
+  const navigate = useNavigate(AuthContext)
 
+  const goToMakeProduct = () => {
+    navigate('/newProduct')
+  }
+
+  return (
+    <Container>
     <div className="p-5 mb-4 bg-light rounded-3">
       <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">Hottest Item!</h1>  
-        <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-        <button className="btn btn-primary btn-lg" type="button">Example button</button>
+        <h1 className="display-5 fw-bold">Sell Your Stuff!</h1>  
+        <p className="col-md-8 fs-4"> Mobi is a marketplace for those who crave something different. Built with small-business owners in mind, Mobi lets you buy or sell unique items. </p>
+        <button className="btn btn-light btn-lg" type="button" onClick={() => goToMakeProduct()}>Sell Something!</button>
       </div>
     </div>
+    </Container>
   )
 }
 
